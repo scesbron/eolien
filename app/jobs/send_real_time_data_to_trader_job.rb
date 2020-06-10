caf85ad367340299ace4d967ebb8d1f196d53fbe
Sebastien Cesbron <sebastien@wecasa.fr>
2020-06-10 14:02:29 +0200

    remove quotes

--- app/jobs/send_real_time_data_to_trader_job.rb
+++ app/jobs/send_real_time_data_to_trader_job.rb
@@ -18,7 +18,7 @@ class SendRealTimeDataToTraderJob < ApplicationJob
   def generate_global_file(wind_farm, session_id, start_time, end_time)
     headers = ['DATE', 'GRID_P (kW)']
 
-    CSV.generate(headers: true, col_sep: ';', force_quotes: true, encoding: Encoding::UTF_8) do |csv|
+    CSV.generate(headers: true, col_sep: ';', encoding: Encoding::UTF_8) do |csv|
       csv << headers
       Scada::Api.parc_ten_minutes_values(session_id, wind_farm, start_time, end_time).reverse.each do |value|
         csv << [
@@ -32,7 +32,7 @@ class SendRealTimeDataToTraderJob < ApplicationJob
   def generate_unit_file(wind_farm, session_id, start_time, end_time)
     headers = ['DATE', 'WTG_Num', 'WTG_GD_P (kW)', 'WTG_OP_ST', 'WTG_WD_SPE (m/s)', 'WTG_WD_DIR (°)']
 
-    CSV.generate(headers: true, col_sep: ';', force_quotes: true, encoding: Encoding::UTF_8) do |csv|
+    CSV.generate(headers: true, col_sep: ';', encoding: Encoding::UTF_8) do |csv|
       csv << headers
       wind_farm.wind_turbines.enabled.each do |wind_turbine|
         Scada::Api.turbine_ten_minutes_values(session_id, wind_turbine, start_time, end_time).reverse.each do |value|
