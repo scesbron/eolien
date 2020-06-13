f92dd6d1afcb7fdc41feea1e266af2dd0cdaed6e
Sebastien Cesbron <sebastien@wecasa.fr>
2020-06-13 14:49:58 +0200

    change the format of trader email to be able to add cc values

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -2,8 +2,9 @@ class JobMailer < ApplicationMailer
 
   def send_real_time_data_to_trader(wind_farm, global_csv, unit_csv)
     common_name = "#{wind_farm.slug.upcase}_#{Time.now.strftime('%Y%m%d_%H%M%S')}"
+    recipients = JSON.parse(wind_farm.trader_email)
     attachments["CNR_LiveData_WF_#{common_name}.csv"] = { mime_type: 'text/csv', content: global_csv }
     attachments["CNR_LiveData_WTG_#{common_name}.csv"] = { mime_type: 'text/csv', content: unit_csv }
-    mail(to: wind_farm.trader_email, subject: wind_farm.trader_mail_subject, body: 'Données temps réel')
+    mail(to: recipients['to'], cc: recipients['cc'], subject: wind_farm.trader_mail_subject, body: 'Données temps réel')
   end
 end
