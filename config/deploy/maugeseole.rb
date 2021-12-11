88984b6a8e6072d4535b79595f3dc083ef8ec81f
Sebastien Cesbron <sebastien@wecasa.fr>
2021-12-11 18:19:09 +0100

    change user for maugeseole

--- config/deploy/maugeseole.rb
+++ config/deploy/maugeseole.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'maugeseole', user: 'rails', roles: %w[app db web]
+server 'maugeseole', user: 'deploy', roles: %w[app db web]
 set :stage, :production
 
 # role-based syntax
