f1e1e0c347d73af1b20536905b78c78b153d54dd
Sebastien Cesbron <sebastien@wecasa.fr>
2021-12-11 18:37:43 +0100

    set the cors origins

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.16.0'
 set :application, 'eolien'
 set :repo_url, 'git@github.com:scesbron/eolien.git'
 
-set :deploy_to, '/home/rails/eolien'
+set :deploy_to, '/home/deploy/eolien'
 
 append :linked_files, '.env.local'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets'
